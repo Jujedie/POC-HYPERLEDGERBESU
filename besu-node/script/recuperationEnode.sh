6be6fe480fbc51684171d30145e3d0f703bcdc74
jujedie <oyer.ludovic@gmail.com>
2025-04-15 11:20:45 +0200

    Update scripts and configuration files: pass P2P_PORT to recuperationEnode.sh, fix timestamp in genesis and qbftConfigFile, and clean up recuperationEnode.sh

--- besu-node/script/recuperationEnode.sh
+++ besu-node/script/recuperationEnode.sh
@@ -1,6 +1,7 @@
 IP=$(hostname -I | cut -d ' ' -f 1)
+echo "IP: $IP" > /opt/besu/data/$1/enodeUrl.txt
 
-while ! docker compose logs | grep -q "Enode URL" log; do sleep 1; done
+while ! docker compose logs | grep -q "Enode URL"; do sleep 1; done
 ENODE=$(docker compose logs | grep "Enode URL" | cut -d '|' -f 5 | cut -d ' ' -f 4)
 echo "$ENODE@$IP:$2" > /opt/besu/data/$1/enodeUrl.txt
 echo "Enode URL: $ENODE"
