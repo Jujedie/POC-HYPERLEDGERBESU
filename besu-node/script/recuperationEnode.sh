b17403dae1d09db9ad4d056c06bb8857a8357272
jujedie <oyer.ludovic@gmail.com>
2025-04-15 10:56:46 +0200

    Update recuperationEnode.sh: change IP retrieval method to use hostname command

--- besu-node/script/recuperationEnode.sh
+++ besu-node/script/recuperationEnode.sh
@@ -1,8 +1,4 @@
-IP=$(ip a | grep -oP '(?<=inet\s)\d+(\.\d+){3}' | sed -n '2p')
-# -oP: option to use Perl regex and output only the matching part
-# '(?<=inet\s)\d+(\.\d+){3}': regex to match the IP address
-# sed -n '2p': print only the second line of the output
-
+IP=$(hostname -I | cut -d ' ' -f 1)
 
 while ! docker compose logs | grep -q "Enode URL" log; do sleep 1; done
 ENODE=$(docker compose logs | grep "Enode URL" | cut -d '|' -f 5 | cut -d ' ' -f 4)
