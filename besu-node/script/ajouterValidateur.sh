5ca42e7ad662b36087191485c5369247c8428f77
jujedie <oyer.ludovic@gmail.com>
2025-04-16 11:00:47 +0200

    Update Prometheus target and enhance validator addition script with content type header

--- besu-node/script/ajouterValidateur.sh
+++ besu-node/script/ajouterValidateur.sh
@@ -2,4 +2,7 @@
 IP=$(cat $1/data/enodeUrl.txt | cut -d '@' -f 2)
 
 address=$(cat $1/data/nodeAddress.txt)
-curl -X POST --data '{"jsonrpc":"2.0","method":"ibft_proposeValidatorVote","params":["'$address'"],"id":1}' -H "Content-Type: application/json" $IP
+curl -X POST --data '{"jsonrpc":"2.0","method":"ibft_proposeValidatorVote","params":["'$address'"],"id":1}'  http://$IP
+
+# Vérifie si le nouveau validateur a été ajouté
+curl -X POST --data '{"jsonrpc":"2.0","method":"ibft_getValidatorsByBlockNumber","params":["latest"], "id":1}' http://$IP
