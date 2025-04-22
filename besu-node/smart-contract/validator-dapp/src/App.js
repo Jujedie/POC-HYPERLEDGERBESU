b5ed7c4aa50ca8c861ff7e9fa647f52d121e7f17
jujedie <oyer.ludovic@gmail.com>
2025-04-22 14:55:18 +0200

    Fix input field IDs and update changeValidateur function for better clarity

--- besu-node/smart-contract/validator-dapp/src/App.js
+++ besu-node/smart-contract/validator-dapp/src/App.js
@@ -7,8 +7,8 @@ function App() {
       <h1>Validator App</h1>
 
       <div>
-        <input type="text" id="stringInput" placeholder="Entrez l'adresse du noeud" />
-        <input type="text" id="stringInput2" placeholder="IPValidateur:portRPC" />
+        <input type="text" id="inputAdress" placeholder="Entrez l'adresse du noeud" />
+        <input type="text" id="inputIpValidateur" placeholder="IPValidateur:portRPC" />
         <select id="booleanInput">
           <option value="true">True</option>
           <option value="false">False</option>
@@ -20,6 +20,27 @@ function App() {
 }
 
 function changeValidateur() {
+  const url = Document.getElementById("inputIpValidateur").value;
+  const address = Document.getElementById("inputAdress").value;
+  const booleanValue = Document.getElementById("booleanInput").value === "true"; // Convert string to boolean
+
+  const requestBody = {
+    jsonrpc: "2.0",
+    method: "qbft_proposeValidatorVote",
+    params: [address, booleanValue],
+    id: 1
+  };
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(requestBody)
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.error("Error:", error));
 }
 
 export default App;
