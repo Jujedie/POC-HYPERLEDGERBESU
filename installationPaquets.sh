e3331e0cd5bf109f8c0548869e4d37607a4b7958
Allan Legrand <allan.legrand16@gmail.com>
2025-04-25 14:32:58 +0200

    modification installation java 21 et nvm

--- installationPaquets.sh
+++ installationPaquets.sh
@@ -2,13 +2,11 @@
 
 sudo apt-get update
 
-# Installation de Java 17
-sudo apt install openjdk-17-jdk 
+# Installation de Java 21 
+wget https://download.bell-sw.com/java/21.0.7+9/bellsoft-jdk21.0.7+9-linux-aarch64.deb
+sudo dpkg --install bellsoft-jdk21*-linux-aarch64.deb
 
-# Installation de besu
-wget https://github.com/hyperledger/besu/releases/download/25.4.0/besu-25.4.0.tar.gz
-sudo tar -xvf besu-25.4.0.tar.gz -C /opt
-sudo mv /opt/besu-25.4.0 /opt/besu
+# Installation de besu wget https://github.com/hyperledger/besu/releases/download/25.4.0/besu-25.4.0.tar.gz sudo tar -xvf besu-25.4.0.tar.gz -C /opt sudo mv /opt/besu-25.4.0 /opt/besu
 echo "export PATH=\$PATH:/opt/besu/bin" >> ~/.bashrc
 
 # Installation de Docker
@@ -37,8 +35,6 @@ echo "Docker installé, veuillez redémarrer votre machine pour que les modifica
 
 # Installation de NodeJS
 
-sudo apt install nvm
-sudo apt install npm
-sudo apt install npx
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 
 source ~/.bashrc
