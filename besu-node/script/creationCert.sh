225b37601c99dde2806ec81bd11b99a5f25cdf98
Allan LEGRAND <allan.legrand16@gmail.com>
2025-04-29 10:32:19 +0200

    modif rpc ajout handle mdp

--- besu-node/script/creationCert.sh
+++ besu-node/script/creationCert.sh
@@ -6,6 +6,3 @@ mkdir -p "$CERT_DIR"
 openssl req -x509 -newkey rsa:4096 -sha256 -days 365 \
     -nodes -keyout "$PRIVKEY" -out "$FULLCHAIN" \
     -subj "/C=FR/ST=France/L=Paris/O=MonOrganisation/OU=IT/CN=localhost"
-
-
-
--- besu-node/script/rpc.sh
+++ besu-node/script/rpc.sh
@@ -18,9 +18,10 @@ fi
 
 echo -n "Password: "
 read -s PASSWORD
+echo ""
 
 # Fetch the authentication token using the provided username and password
-TOKEN=$(curl -X POST --data '{"username":"'"$1"'","password":"'"$PASSWORD"'"}' http://localhost:8545/login | jq .token | tr -d '"')
+TOKEN=$(curl -X POST --data '{"username":"'"$1"'","password":"'"$PASSWORD"'"}' http://localhost:8545/login 2>/dev/null | jq .token 2>/dev/null | tr -d '"')
 
 # Check if token retrieval was successful
 if [[ -z "$TOKEN" ]]; then
@@ -35,4 +36,4 @@ params_str=$(IFS=,; echo "[${params[*]}]")
 IP_EXTERNE=$(cat ../.env | grep IP_EXTERNE | cut -d '=' -f 2)
 RPC_PORT=$(cat ../.env | grep RPC_PORT | cut -d '=' -f 2)
 # Call the JSON-RPC method and display the result
-curl -X POST -H "Authorization: Bearer $TOKEN" --data '{"jsonrpc":"2.0","method":"'"$2"'","params":'"$params_str"',"id":1}' http://$IP_EXTERNE:$RPC_PORT | jq
+curl -X POST -H "Authorization: Bearer $TOKEN" --data '{"jsonrpc":"2.0","method":"'"$2"'","params":'"$params_str"',"id":1}' http://$IP_EXTERNE:$RPC_PORT 2>/dev/null | jq .result
