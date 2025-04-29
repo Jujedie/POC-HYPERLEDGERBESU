4041e77756b378e65cd380ac4b5ed82f81e7a599
Allan LEGRAND <allan.legrand16@gmail.com>
2025-04-29 11:18:46 +0200

    ajout parametre rpc

--- besu-node/script/rpc.sh
+++ besu-node/script/rpc.sh
@@ -1,27 +1,63 @@
 #!/bin/bash
 
-# Display usage/help if -h or --help is provided
-if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
-  echo "Usage: $0 <login> <method> [params]"
+# Function to display usage/help
+usage() {
+  echo "Usage: $0 [--ip <ip>] [--rpc-port <port>] <login> <method> [params] "
   echo "  <login>    - The username for authentication"
   echo "  <method>   - The JSON-RPC method to call"
-  echo "  [params]   - Optional parameters for the JSON-RPC method (can be passed as space-separated values) (if the params is a string, put in between quote like this '\"some params\"')"
+  echo "  [params]   - Optional parameters for the JSON-RPC method (can be passed as space-separated values)"
+  echo "  [--ip]     - Optional IP address (default from .env if not provided)"
+  echo "  [--rpc-port] - Optional RPC port (default from .env if not provided)"
   exit 0
+}
+
+# Display usage/help if -h or --help is provided
+if [[ "$1" == "-h" ]] || [[ "$1" == "--help" ]]; then
+  usage
 fi
 
 # Ensure at least 2 arguments (login, method) are provided
 if [[ $# -lt 2 ]]; then
   echo "Error: Missing arguments."
-  echo "Usage: $0 <login> <password> <method> [params]"
+  usage
+fi
+
+# Parse optional IP and RPC port
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    --ip|-i)
+      IP_EXTERNE="$2"
+      shift 2
+      ;;
+    --rpc-port|-r)
+      RPC_PORT="$2"
+      shift 2
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+# Set default values for IP and RPC port if not provided
+if [[ -z "$IP_EXTERNE" ]]; then
+  IP_EXTERNE=$(cat ../.env | grep IP_EXTERNE | cut -d '=' -f 2)
+fi
+
+if [[ -z "$RPC_PORT" ]]; then
+  RPC_PORT=$(cat ../.env | grep RPC_PORT | cut -d '=' -f 2)
+fi
+
+# Check if IP and RPC port are set
+if [[ -z "$IP_EXTERNE" ]] || [[ -z "$RPC_PORT" ]]; then
+  echo "Error: IP or RPC port not set and could not be found in .env."
   exit 1
 fi
 
+# Prompt for password
 echo -n "Password: "
 read -s PASSWORD
 echo ""
-
-IP_EXTERNE=$(cat ../.env | grep IP_EXTERNE | cut -d '=' -f 2)
-RPC_PORT=$(cat ../.env | grep RPC_PORT | cut -d '=' -f 2)
 
 # Fetch the authentication token using the provided username and password
 TOKEN=$(curl -X POST --data '{"username":"'"$1"'","password":"'"$PASSWORD"'"}' http://$IP_EXTERNE:$RPC_PORT/login 2>/dev/null | jq .token 2>/dev/null | tr -d '"')
@@ -38,3 +74,4 @@ params_str=$(IFS=,; echo "[${params[*]}]")
 
 # Call the JSON-RPC method and display the result
 curl -X POST -H "Authorization: Bearer $TOKEN" --data '{"jsonrpc":"2.0","method":"'"$2"'","params":'"$params_str"',"id":1}' http://$IP_EXTERNE:$RPC_PORT 2>/dev/null | jq .result
+
