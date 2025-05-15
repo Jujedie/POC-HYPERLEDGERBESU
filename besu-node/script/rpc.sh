271041d526bb8adc0f3a6c0090bb56ea8364a31e
jujedie <oyer.ludovic@gmail.com>
2025-05-15 09:59:26 +0200

    Update token retrieval URL in rpc.sh to use dynamic IP and RPC port

--- besu-node/script/rpc.sh
+++ besu-node/script/rpc.sh
@@ -57,7 +57,7 @@ fi
 echo -n "Password: "
 read -s PASSWORD
 
-TOKEN=$(curl -X POST --data '{"username":"'"$1"'","password":"'"$PASSWORD"'"}' http://localhost:8545/login 2>/dev/null | jq .token 2>/dev/null | tr -d '"')
+TOKEN=$(curl -X POST --data '{"username":"'"$1"'","password":"'"$PASSWORD"'"}' http://$IP_EXTERNE:$RPC_PORT/login 2>/dev/null | jq .token 2>/dev/null | tr -d '"')
 
 echo -e "Token retrieved: $TOKEN \n"
 
