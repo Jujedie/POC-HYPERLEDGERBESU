c85c049946ab5aca057dbcc4df461ca4460e9527
jujedie <oyer.ludovic@gmail.com>
2025-05-16 10:43:57 +0200

    Refactor connection form and script for improved functionality and consistency

--- website/scripts/connexion.js
+++ website/scripts/connexion.js
@@ -1,23 +1,29 @@
-document.getElementById('formConnect').addEventListener('submit', function(event) {
-	const id  = document.getElementById("id").value;
-	const mdp = document.getElementById("mdp").value;
-	const ip  = document.getElementById("ip").value;
+document.getElementById('formConnect').addEventListener('submit', async function(event) {
+	const id  = document.getElementById("ID").value;
+	const mdp = document.getElementById("MDP").value;
+	const ip  = document.getElementById("IP").value;
+
+	console.log("ID:", id);
+	console.log("MDP:", mdp);
+	console.log("IP:", ip);
 
 	sessionStorage.setItem("id", id);
 	sessionStorage.setItem("mdp", mdp);
 	sessionStorage.setItem("ip", ip);
 	event.preventDefault();
 
-	if ( fetchToken() == false) {
-		document.getElementById("id").value = id;
-		document.getElementById("mdp").value = mdp;
-		document.getElementById("ip").value = ip;
+	let tokenValid = await fetchToken();
+	console.log("Token valid:", tokenValid);
+	if (tokenValid === false) {
+		document.getElementById("ID").value = id;
+		document.getElementById("MDP").value = mdp;
+		document.getElementById("IP").value = ip;
 	} else {
 		window.location.href = "../index.html";
 	}
 });
 
-function fetchToken() {
+async function fetchToken() {
 	const url = "http://" + sessionStorage.getItem("ip");
 
 	const loginBody = {
@@ -25,34 +31,31 @@ function fetchToken() {
 		password: sessionStorage.getItem("mdp"),
 	};
 
-	fetch(`${url}/login`, {
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify(loginBody),
-	})
-		.then((response) => {
-			if (!response.ok) {
-				throw new Error(`HTTP error! status: ${response.status}`);
-			}
-			return response.json();
-		})
-		.then((data) => {
-			if (data.token) {
-				console.log("Token fetched:", data.token);
-				sessionStorage.setItem("token", data.token);
-				return true;
-			} else {
-				throw new Error("Failed to fetch token");
-			}
-		})
-		.catch((error) => {
-			console.error("Error fetching token:", error);
-
-			sessionStorage.removeItem("id");
-			sessionStorage.removeItem("mdp");
-			sessionStorage.removeItem("ip");
-			return false;
+	try {
+		const response = await fetch(`${url}/login`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(loginBody),
 		});
+		if (!response.ok) {
+			throw new Error(`HTTP error! status: ${response.status}`);
+		}
+		const data = await response.json();
+		if (data.token) {
+			console.log("Token fetched:", data.token);
+			sessionStorage.setItem("token", data.token);
+			return true;
+		} else {
+			throw new Error("Failed to fetch token");
+		}
+	} catch (error) {
+		console.error("Error fetching token:", error);
+
+		sessionStorage.removeItem("id");
+		sessionStorage.removeItem("mdp");
+		sessionStorage.removeItem("ip");
+		return false;
+	}
 }
