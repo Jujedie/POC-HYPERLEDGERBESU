c02cd19559ff8a6837f0d07baa152061bedcc695
Allan Legrand <allan.legrand16@gmail.com>
2025-06-02 09:59:37 +0200

    suppression temporaire des smart contract pour push gitlab

--- DApps/smart-contracts/ignition/modules/IncredibleStorage.js
+++
@@ -1,6 +0,0 @@
-const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
-
-module.exports = buildModule("IncredibleStorageModule", (m) => {
-	const incredibleStorage = m.contract("IncredibleStorage", [42], { });
-	return { incredibleStorage };
-});
